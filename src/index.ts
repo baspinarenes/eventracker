4e2c6588771de3a03bd2e354d2ecb082c5398ec5
Enes Başpınar <ensbaspinar@gmail.com>
2024-07-14 17:38:54 +0300

    Add global event handler for plain js

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
-export function sayHello() {
-  console.log("Hello, world!");
+export function registerEventTracker(
+  eventTrackerRootId: string,
+  callback: (action: string, eventName: string, payload: any) => void
+) {
+  console.log("asdasdasdsa");
+  document.addEventListener("DOMContentLoaded", () => {
+    const element = document.getElementById(eventTrackerRootId);
+
+    if (element) {
+      element.addEventListener("eventracker", (e) => {
+        e.stopPropagation();
+        const { action, eventName, payload } = (e as CustomEvent).detail;
+        callback(action, eventName, payload);
+      });
+    }
+  });
 }

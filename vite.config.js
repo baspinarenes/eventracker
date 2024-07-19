acc2efed8342b531c14f9716f5d78e9eb796ec53
Enes Başpınar <ensbaspinar@gmail.com>
2024-07-19 08:02:53 +0300

    Add plugins to vite for reduce bundle size

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,19 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 import react from "@vitejs/plugin-react";
+import resolve from "@rollup/plugin-node-resolve";
+import external from "rollup-plugin-peer-deps-external";
+import terser from "@rollup/plugin-terser";
 
 export default defineConfig({
   base: "./",
-  plugins: [dts({ rollupTypes: true }), react()],
+  plugins: [react(), external(), dts({ rollupTypes: true }), terser()],
   build: {
     sourcemap: true,
     lib: {
-      entry: [
-        resolve(__dirname, "src/index.ts"),
-        resolve(__dirname, "src/react.ts"),
-      ],
+      entry: [resolve(__dirname, "src/index.ts"), resolve(__dirname, "src/react.ts")],
       name: "eventracker",
-      fileName: (format, entryName) =>
-        format === "cjs" ? `${entryName}.cjs` : `${entryName}.js`,
+      fileName: (format, entryName) => (format === "cjs" ? `${entryName}.cjs` : `${entryName}.js`),
       formats: ["es", "cjs"],
     },
     rollupOptions: {
